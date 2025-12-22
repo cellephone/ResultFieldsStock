8c5958d9934eeeef8edad57d5eb77cc7fba96c5b
cellephone <a.fischer@cellephone.de>
2025-12-22 21:47:22 +0100

    Add files via upload

---
+++ src/Providers/ResultFieldsStockServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+namespace ResultFieldsStock\Providers;
+use Plenty\Plugin\ServiceProvider;
+
+class ResultFieldsStockServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+}
