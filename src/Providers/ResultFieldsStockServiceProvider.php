2a259e4b3ab07025208b6821c2308eae7ffa79ab
cellephone <a.fischer@cellephone.de>
2025-12-24 17:35:03 +0100

    Update ResultFieldsStockServiceProvider.php

--- src/Providers/ResultFieldsStockServiceProvider.php
+++ src/Providers/ResultFieldsStockServiceProvider.php
@@ -6,5 +6,11 @@ class ResultFieldsStockServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        // Registriere die ResultFields aus diesem Plugin
+        $this->getApplication()->register('Plenty.resultFields.stock', function($app) {
+            return [
+                'SingleItem' => __DIR__ . '/../resources/views/ResultFields/SingleItem.fields.json'
+            ];
+        });
     }
 }
