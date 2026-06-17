51e414beec268d49c3fcdb3691cd47f41f7c3179
cellephone <a.fischer@cellephone.de>
2026-06-17 18:48:00 +0200

    Update ResultFieldsStockServiceProvider.php

--- src/Providers/ResultFieldsStockServiceProvider.php
+++ src/Providers/ResultFieldsStockServiceProvider.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace ResultFieldsStock\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+use IO\Services\ItemSearch\Helper\ResultFieldTemplate;
+
+class ResultFieldsStockServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+        pluginApp(ResultFieldTemplate::class)
+            ->addResultField('ResultFieldsStock::ResultFields.SingleItem');
+    }
+}
